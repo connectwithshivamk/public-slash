5ecd692061fdd814cf321db255dab85dfc289eec
Shivam Katiyar <deployewithapp0011@gmail.com>
2025-10-31 16:57:40 +0530

    Delete utils directory

--- utils/publicModel.js
+++
@@ -1,7 +0,0 @@
-exports.generateComponent = function(prompt) {
-  return `// Public fallback model
-// This just creates a simple button based on the prompt: "${prompt}"
-export default function Button() {
-  return <button style={{padding: '8px 16px', borderRadius: '6px'}}>Public Button</button>;
-}`;
-};
